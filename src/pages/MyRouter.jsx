88947017bbe91982bff783822667f5a93914eb7c
Haixiang6123 <haixiang6123@gmail.com>
2018-09-24 14:49:36 -0700

    Update: switch to hash history router

--- src/pages/MyRouter.jsx
+++ src/pages/MyRouter.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 
 import Home from './Home';
 import Result from './Result';
